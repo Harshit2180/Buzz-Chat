4f3ff2e73b2908db054b26d9bc0c4fc9c2db7828
Harshit2180 <chopraharshit2180@gmail.com>
2025-12-31 14:32:48 +0530

    Create socket connection in frontend and backend and persist redux store

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import WelcomeScreen from "./components/WelcomeScreen";
 import Chat from "./pages/Chat";
+import { useDispatch, useSelector } from "react-redux";
+import io from 'socket.io-client'
+import { setSocket } from "./Redux/socketSlice";
+import { setOnlineUsers } from "./Redux/userSlice";
 
 const appRouter = createBrowserRouter([
   {
@@ -21,6 +24,33 @@ const appRouter = createBrowserRouter([
 ])
 
 const App = () => {
+
+  const { authUser } = useSelector(store => store.user)
+  const { socket } = useSelector(store => store.socket)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io('http://localhost:8000', {
+        query: {
+          userId: authUser._id
+        }
+      })
+      dispatch(setSocket(socket))
+
+      socket.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers))
+      })
+
+      return () => socket.close()
+    }
+    else {
+      if (socket) {
+        socket.close()
+        dispatch(setSocket(null))
+      }
+    }
+  }, [authUser])
 
   return (
     <div>
